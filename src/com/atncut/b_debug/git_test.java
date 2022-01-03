891d9050f6b64cb79194c712ea04abcc90867282
xiaoxiaowu99 <905352004@qq.com>
2022-01-03 13:36:13 +0800

    second comiit after add

--- src/com/atncut/b_debug/git_test.java
+++ src/com/atncut/b_debug/git_test.java
@@ -3,5 +3,6 @@ package com.atncut.b_debug;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("ceshi");
+        System.out.println("second");
     }
 }
