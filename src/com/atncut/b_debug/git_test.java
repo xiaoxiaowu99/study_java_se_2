7320b3440586fb45b1b756a0b7de0991c79fdb8e
xiaoxiaowu99 <905352004@qq.com>
2022-01-03 14:08:05 +0800

    hotfix_commit12

--- src/com/atncut/b_debug/git_test.java
+++ src/com/atncut/b_debug/git_test.java
@@ -4,5 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("ceshi");
         System.out.println("second");
+        System.out.println("hot_fix222");
+        System.out.println("hot_fix222");
     }
 }
